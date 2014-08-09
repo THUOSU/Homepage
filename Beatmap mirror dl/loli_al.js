d9021280b94301c30cc75a5a51501c7c5b23ce3f
crucifyml <crucify_m_l@163.com>
2014-08-09 20:31:56 +0800

    modify the code for general use.

--- Beatmap mirror dl/loli_al.js
+++ Beatmap mirror dl/loli_al.js
@@ -1,1 +1,7 @@
-javascript:(function(b,c){c=b.pathname.match(/[bus]\/.+/);if(b.host=='osu.ppy.sh'&&c){b.href='http://loli.al/s/'+(/return play\((\d+)/.exec(document.body.innerHTML) [1])}})(window.location)
+javascript:
+(function(b){
+	if(b.host=="osu.ppy.sh"&&b.pathname.match(/\/[bs]\/.+/)){
+		songid=$('.beatmapDownloadButton:last a').attr('href').match(/\d+/);
+		b.href="http://loli.al/s/"+songid;
+	}
+})(window.location);
--- Beatmap mirror dl/osu_mengsky_net.js
+++ Beatmap mirror dl/osu_mengsky_net.js
@@ -1,1 +1,7 @@
-javascript:(function(b,c){c=b.pathname.match(/[bus]\/.+/);if(b.host=='osu.ppy.sh'&&c){b.href='http://osu.mengsky.net/d.php?id='+(/return play\((\d+)/.exec(document.body.innerHTML) [1])}})(window.location)
+javascript:
+(function(b){
+	if(b.host=="osu.ppy.sh"&&b.pathname.match(/\/[bs]\/.+/)){
+		songid=$('.beatmapDownloadButton:last a').attr('href').match(/\d+/);
+		b.href="http://osu.mengsky.net/d.php?id="+songid;
+	}
+})(window.location);
