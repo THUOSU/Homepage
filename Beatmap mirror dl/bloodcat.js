893f1de78d4d80838469343542c7c7fe3c5af1ef
crucifyml <crucify_m_l@163.com>
2014-08-09 16:04:33 +0800

    Compress the codes. Add loli.al and bloodcat.

---
+++ Beatmap mirror dl/bloodcat.js
@@ -0,0 +1,1 @@
+javascript:(function(b,c){c=b.pathname.match(/[bus]\/.+/);if(b.host=='osu.ppy.sh'&&c){b.href='http://bloodcat.com/osu/m/'+(/return play\((\d+)/.exec(document.body.innerHTML) [1])}})(window.location)
--- Beatmap mirror dl/osu_mengsky_net.js
+++ Beatmap mirror dl/osu_mengsky_net.js
@@ -1,7 +1,1 @@
-javascript:(function(b,c){
-	c=b.pathname.match(/[bus]\/.+/);
-	if(b.host=='osu.ppy.sh'&&c){
-		b.href='http://osu.mengsky.net/d.php?id='+(/return play\((\d+)/.exec(document.body.innerHTML) [1])
-	}
-})
-(window.location)
+javascript:(function(b,c){c=b.pathname.match(/[bus]\/.+/);if(b.host=='osu.ppy.sh'&&c){b.href='http://osu.mengsky.net/d.php?id='+(/return play\((\d+)/.exec(document.body.innerHTML) [1])}})(window.location)
