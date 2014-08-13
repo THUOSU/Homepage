7ea3b0a5b5820fac6993682a71c39f37a6f8005a
Glaceon31 <grit31@126.com>
2014-08-13 18:07:11 +0800

    update to suit new bp page

--- tools/ppIndexer.py
+++ tools/ppIndexer.py
@@ -4,6 +4,7 @@ import time
 import string
 import json
 
+
 userNum = 10000
 userPerPage = 50
 maxpp = 600
@@ -11,6 +12,43 @@ sleeptime = 0.02
 
 url_performance = "http://osu.ppy.sh/p/pp/?page="
 url_bp= "http://osu.ppy.sh/pages/include/profile-leader.php?u="
+
+index = range(0, maxpp+1)
+for i in range(0, maxpp+1):
+    index[i] = {}
+
+def fetchplayer(playerID):
+    #get best performance, get rid of first place ranks
+    bpresponse = urllib2.urlopen(url_bp+playerID).read()
+    
+    p = re.compile("Best .*?Performance([\S\s]*?)First Place Ranks")
+    bpresponse = p.findall(bpresponse)[0]
+    
+    pmap = re.compile('<b>(<a href=.*?</a>).*?</b>')
+    mapresult = pmap.findall(bpresponse)
+    #print len(mapresult)
+    pp = re.compile("<b>(\d+)pp</b>")
+    ppresult = pp.findall(bpresponse)
+    #print len(ppresult)
+    weight = re.compile("weighted <b>(\d+)%</b>")
+    weightresult = weight.findall(bpresponse)
+    #print len(weightresult)
+    time.sleep(sleeptime)
+    #indexer
+    if len(mapresult) == 50 and len(ppresult) == 50 and len(weightresult) == 50:
+        for i in range(0, 50):
+            mapvalue = mapresult[i]
+            ppvalue = string.atoi(ppresult[i])
+            weightvalue = string.atoi(weightresult[i])
+            if ppvalue > maxpp:
+                continue
+            if not index[ppvalue].has_key(mapvalue):
+                index[ppvalue][mapvalue] = weightvalue
+            else:
+                index[ppvalue][mapvalue] += weightvalue
+
+
+
 
 #fetch top player
 playerID_list = []
@@ -25,44 +63,17 @@ for i in range(1, userNum/userPerPage+1) :
     time.sleep(2*sleeptime)
     
 
-
-index = range(0, maxpp+1)
-for i in range(0, maxpp+1):
-    index[i] = {}
-
 #playerID_list = ['352328']
 print 'indexing...'
 num = 0
 for playerID in playerID_list :
     num += 1
     print str(num)
-    #get best performance, get rid of first place ranks
-    bpresponse = urllib2.urlopen(url_bp+playerID).read()
-    p = re.compile("Best .*?Performance([\S\s]*?)First Place Ranks")
-    bpresponse = p.findall(bpresponse)[0]
-
-    pmap = re.compile('<b>(<a href=.*?</a>)</b>')
-    mapresult = pmap.findall(bpresponse)
-
-    pp = re.compile("<b>(\d+)pp</b>")
-    ppresult = pp.findall(bpresponse)
-
-    weight = re.compile("weighted <b>(\d+)%</b>")
-    weightresult = weight.findall(bpresponse)
+    fetchplayer(playerID)
     
-    time.sleep(sleeptime)
-    #indexer
-    if len(mapresult) == 50 and len(ppresult) == 50 and len(weightresult) == 50:
-        for i in range(0, 50):
-            mapvalue = mapresult[i]
-            ppvalue = string.atoi(ppresult[i])
-            weightvalue = string.atoi(weightresult[i])
-            if ppvalue > maxpp:
-                continue
-            if not index[ppvalue].has_key(mapvalue):
-                index[ppvalue][mapvalue] = weightvalue
-            else:
-                index[ppvalue][mapvalue] += weightvalue
+
+#fetchplayer('352328')
+#print index
 
 output = open('ppIndex.txt', 'w')
 output.write(json.dumps(index))
