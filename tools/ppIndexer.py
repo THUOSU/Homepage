c3983bbced01005a16fe5310b8626138e83150f8
Glaceon31 <grit31@126.com>
2014-08-13 21:11:29 +0800

    use multithread to accelerate indexer

--- tools/ppIndexer.py
+++ tools/ppIndexer.py
@@ -3,12 +3,15 @@ import re
 import time
 import string
 import json
-
+import thread
+import threading
 
 userNum = 10000
 userPerPage = 50
 maxpp = 600
-sleeptime = 0.02
+sleeptime = 0.5
+fetchlock = threading.RLock()
+indexlock = threading.RLock()
 
 url_performance = "http://osu.ppy.sh/p/pp/?page="
 url_bp= "http://osu.ppy.sh/pages/include/profile-leader.php?u="
@@ -33,9 +36,10 @@ def fetchplayer(playerID):
     weight = re.compile("weighted <b>(\d+)%</b>")
     weightresult = weight.findall(bpresponse)
     #print len(weightresult)
-    time.sleep(sleeptime)
+    
     #indexer
     if len(mapresult) == 50 and len(ppresult) == 50 and len(weightresult) == 50:
+        indexlock.acquire()
         for i in range(0, 50):
             mapvalue = mapresult[i]
             ppvalue = string.atoi(ppresult[i])
@@ -46,31 +50,57 @@ def fetchplayer(playerID):
                 index[ppvalue][mapvalue] = weightvalue
             else:
                 index[ppvalue][mapvalue] += weightvalue
+        finish_list.append(playerID)
+        indexlock.release()
 
-
+def fetchperformance(i):
+    #print url_performance+str(i)
+    response = urllib2.urlopen(url_performance+str(i)).read()
+    p = re.compile("<td><img class='flag' src=.*?(\d+).*?</a></td>")
+    m = p.findall(response)
+    fetchlock.acquire()
+    for i in m:
+        playerID_list.append(i)
+    fetchlock.release()
 
 
 #fetch top player
 playerID_list = []
-print 'fetching...'
+print 'start fetching...'
 for i in range(1, userNum/userPerPage+1) :
-    print str(i*50)
-    response = urllib2.urlopen(url_performance+str(i)).read()
-    p = re.compile("<td><img class='flag' src=.*?(\d+).*?</a></td>")
-    m = p.findall(response)
-    for i in m:
-        playerID_list.append(i)
-    time.sleep(2*sleeptime)
-    
+    thread.start_new_thread(fetchperformance, (i,))
+    if i % 20 == 0:
+        print str(i*50)
+    time.sleep(sleeptime)
+
+print 'waiting for threads to exit...'
+count = 0
+while len(playerID_list) < userNum:
+    print 'finished: '+str(len(playerID_list))+'  '+str(count)+' ticks'
+    time.sleep(5)
+    count = count+1
+    if count > 20:
+        break
 
 #playerID_list = ['352328']
-print 'indexing...'
+print 'start indexing...'
 num = 0
+finish_list = []
 for playerID in playerID_list :
     num += 1
-    print str(num)
-    fetchplayer(playerID)
-    
+    if num % 100 == 0:
+        print str(num)
+    thread.start_new_thread(fetchplayer, (playerID,))
+    time.sleep(sleeptime)
+
+print 'waiting for threads to exit...'
+count = 0
+while len(finish_list) < userNum:
+    print 'finished: '+str(len(finish_list))+'  '+str(count)+' ticks'
+    time.sleep(5)
+    count = count+1
+    if count > 20:
+        break
 
 #fetchplayer('352328')
 #print index
